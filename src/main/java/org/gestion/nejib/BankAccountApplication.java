2db5a3af6617547e5e5c3a5907c1b3f11ba7887d
mnbelgasmi <belgasmi@gmail.com>
2016-03-28 00:06:02 +0200

    From Eclipse

---
+++ src/main/java/org/gestion/nejib/BankAccountApplication.java
@@ -0,0 +1,12 @@
+package org.gestion.nejib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountApplication.class, args);
+	}
+}
